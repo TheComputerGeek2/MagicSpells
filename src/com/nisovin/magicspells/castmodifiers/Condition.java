87d4f827967cfbcb3f50ea2e6d93adafa3503af0
Rifle D. Luffy <theproplayer2@gmail.com>
2018-02-01 08:59:33 +0300

    Update Condition.java

--- src/com/nisovin/magicspells/castmodifiers/Condition.java
+++ src/com/nisovin/magicspells/castmodifiers/Condition.java
@@ -162,7 +162,7 @@ public abstract class Condition {
 		conditions.put("thundering", ThunderingCondition.class);
 		conditions.put("raining", RainingCondition.class);
 		conditions.put("onleash", OnLeashCondition.class);
-		conditions.put("varstringequalscondition", VarStringEqualsCondition.class);
+		conditions.put("varstringequals", VarStringEqualsCondition.class);
 	}
 	
 }
