de394b45dad4628a3ba09146ac5bfae3821a25cf
niblexis <Niblexis@gmail.com>
2019-09-04 13:41:18 -0700

    Fixed "inregion" modifier for 1.14

--- core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/InRegionCondition.java
+++ core/src/main/java/com/nisovin/magicspells/castmodifiers/conditions/InRegionCondition.java
@@ -1,16 +1,18 @@
 package com.nisovin.magicspells.castmodifiers.conditions;
 
 import com.nisovin.magicspells.util.compat.CompatBasics;
-//import org.bukkit.Bukkit;
+import com.sk89q.worldedit.bukkit.BukkitAdapter;
+import com.sk89q.worldedit.world.World;
+import com.sk89q.worldguard.WorldGuard;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.regions.RegionContainer;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
-//import org.bukkit.World;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 import com.nisovin.magicspells.castmodifiers.Condition;
-//import com.sk89q.worldedit.Vector;
 import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
-//import com.sk89q.worldguard.protection.managers.RegionManager;
 import com.sk89q.worldguard.protection.regions.ProtectedRegion;
 
 public class InRegionCondition extends Condition {
@@ -19,14 +21,14 @@ public class InRegionCondition extends Condition {
 	String worldName;
 	String regionName;
 	ProtectedRegion region;
-	
+
 	@Override
 	public boolean setVar(String var) {
 		if (var == null) return false;
-		
+
 		worldGuard = (WorldGuardPlugin)CompatBasics.getPlugin("WorldGuard");
 		if (worldGuard == null || !worldGuard.isEnabled()) return false;
-		
+
 		String[] split = var.split(":");
 		if (split.length == 2) {
 			worldName = split[0];
@@ -48,18 +50,22 @@ public class InRegionCondition extends Condition {
 
 	@Override
 	public boolean check(Player player, Location location) {
-		/*if (region == null) {
-			World world = Bukkit.getWorld(worldName);
+		if (region == null) {
+			org.bukkit.World world = Bukkit.getWorld(worldName);
+
 			if (world == null) return false;
 			if (!world.equals(location.getWorld())) return false;
-			RegionManager regionManager = worldGuard.getRegionManager(world);
+
+			World aWorld = BukkitAdapter.adapt(world);
+
+			RegionContainer regionContainer = WorldGuard.getInstance().getPlatform().getRegionContainer();
+			RegionManager regionManager = regionContainer.get(aWorld);
+
 			if (regionManager == null) return false;
 			region = regionManager.getRegion(regionName);
 		}
 		if (region == null) return false;
-		return region.contains(new Vector(location.getX(), location.getY(), location.getZ()));*/
-		// FIXME update this
-		return false;
+		return region.contains(location.getBlockX(), location.getBlockY(), location.getBlockZ());
 	}
 
 }
